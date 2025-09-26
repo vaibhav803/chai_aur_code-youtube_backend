e01b00dfa50e03f739d0184a19cc4576830ee087
gitcoder <yo@mail.com>
2025-09-26 15:51:53 +0530

    onnect to mongoatlas and env with module type

---
+++ src/app.js
@@ -0,0 +1,38 @@
+// require('dotenv').config({path: './env'})
+
+// import dotenv from "dotenv"
+// import connectDB from "./db"
+
+// dotenv.config({
+//     path: '/.env'
+// })
+
+
+// connectDB()
+
+
+
+/*
+import express from 'express'
+const app = express()
+//iffy
+
+;(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}`)
+        app.on("error",(error) => {
+            console.log("Error: ",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,() =>{
+            console.log(`App is listening on PORT ${PORT}`)
+        })
+    }
+    catch(error){
+        console.error("Error: ",error)
+        throw err
+    }
+})()
+
+*/
