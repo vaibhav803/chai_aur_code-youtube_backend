ccb33124dcd21560f47dcb6f0e154e52b5fc7218
gitcoder <yo@mail.com>
2025-09-28 12:29:51 +0530

    added register-controller and bug fixes

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -51,7 +51,7 @@ const userSchema = new Schema({
 )
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password,10) 
+    this.password = await bcrypt.hash(this.password,10) 
     next()
 })
 
@@ -69,7 +69,7 @@ userSchema.methods.generateAccessToken = function(){
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn : process.env.ACCESS_TOKEN_EXPIRY~
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY,
         }
     )
 }
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -7,3 +7,6 @@ class ApiResponse {
 
     }
 }
+
+
+export { ApiResponse }
